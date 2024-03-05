4fbea0e33d4e5cc5b430fef03f991443517a4230
TeletabiTrifasico <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-03-05 16:45:41 +0100

    Add files via upload

---
+++ assignment3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number of seconds: ");
+            string input = Console.ReadLine();
+            int numberOfSeconds = int.Parse(input);
+
+            int hours = numberOfSeconds / 3600;
+            numberOfSeconds = numberOfSeconds - hours * 3600;
+
+            int minutes = numberOfSeconds / 60;
+            numberOfSeconds = numberOfSeconds - minutes * 60;
+
+            Console.WriteLine($"{hours:00}:{minutes:00}:{numberOfSeconds:00}");
+        }
+    }
+}
